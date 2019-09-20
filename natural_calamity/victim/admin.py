5e119548ae4bf53a3440129e734f7e0e6edd8e09
RAVI <ravinalawade5@gmail.com>
2019-09-20 13:48:57 +0530

    models

--- natural_calamity/victim/admin.py
+++ natural_calamity/victim/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Victim
+from .models import Volunteer
+admin.site.register(Victim)
+admin.site.register(Volunteer)
 # Register your models here.

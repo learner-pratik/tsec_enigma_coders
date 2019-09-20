9b80ca1e91be0a5233a01f3d718c41bf7329d84d
Pratik Naik <pratiknaik4799@gmail.com>
2019-09-20 13:44:30 +0530

    Created form

--- natural_calamity/victim/views.py
+++ natural_calamity/victim/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def login(request):
+    return render(request, 'victim/login.html')
